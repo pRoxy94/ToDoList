d8b8bd246d75ca4b38c2e61071ad3f223dfb88f6
Rossana Laricchia <rossana.laricchia@gmail.com>
2022-05-25 12:25:20 +0200

    Modified Item Model and save/get methods in ListViewModel

--- ToDoList/Views/ViewModels/ListViewModel.swift
+++ ToDoList/Views/ViewModels/ListViewModel.swift
@@ -19,19 +19,34 @@ import Foundation
 
 class ListViewModel: ObservableObject {
     
-    @Published var items: [Item] = []
+    @Published var items: [Item] = [] {
+	   // gets called anytime  we set this items, anytime we change the items array
+	   didSet {
+		  saveItems()
+	   }
+    }
+    
+    
+    let itemsKey: String = "item_list"
     
     init() {
 	   getItems()
     }
     
     func getItems() {
-	   let newItems = [
-		  Item(title: "This is the first title", isCompleted: false),
-		  Item(title: "This is the second", isCompleted: true),
-		  Item(title: "Third!", isCompleted: false)
-	   ]
-	   items.append(contentsOf: newItems)
+//	   let newItems = [
+//		  Item(title: "This is the first title", isCompleted: false),
+//		  Item(title: "This is the second", isCompleted: true),
+//		  Item(title: "Third!", isCompleted: false)
+//	   ]
+//	   items.append(contentsOf: newItems)
+	   
+	   guard
+		  let data = UserDefaults.standard.data(forKey: itemsKey),
+		  let savedItems = try? JSONDecoder().decode([Item].self, from: data)
+	   else { return }
+	   
+	   self.items = savedItems
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -55,6 +70,12 @@ class ListViewModel: ObservableObject {
 	   // is the same as the lines above
 	   if let index = items.firstIndex(where: { $0.id == item.id }) {
 		  items[index] = item.updateCompletion()
+	   }
+    }
+    
+    func saveItems() {
+	   if let encodedData = try? JSONEncoder().encode(items) {
+		  UserDefaults.standard.set(encodedData, forKey: itemsKey)
 	   }
     }
 }
